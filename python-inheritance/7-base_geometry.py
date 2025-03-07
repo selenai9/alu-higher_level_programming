14c04f3d86b40d76b79e967651298cb4101c6857
selenai9 <s.isimbi2@alustudent.com>
2025-03-07 04:32:25 -0800

    k

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Class that defines the attributes of Geometric Shapes."""
+"""Defines a class BaseGeometry."""
 
 
 class BaseGeometry:
-    """Class that defines the attributes of Geometric Shapes."""
+    """A class to define geometric shapes."""
 
     def area(self):
-        """Method that defines the area of a geometric shape."""
+        """Raises an exception as area is not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
-        Method that receives the value property.
+        Validates the value.
 
         Args:
             name (str): name of the object.
@@ -21,7 +21,7 @@ class BaseGeometry:
             TypeError: If value is not an integer.
             ValueError: If value is less or equal to 0.
         """
-        if not isinstance(value, int):
+        if isinstance(value, bool) or not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
