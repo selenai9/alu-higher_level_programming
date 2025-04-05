1d941d79f427241dc630f8764ce166ab795d5a0d
selenai9 <s.isimbi2@alustudent.com>
2025-04-05 21:11:05 +0000

    fds

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,26 +1,13 @@
 #!/usr/bin/python3
-""" lists all State objects from the database """
+'''class definition of a state'''
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+Base = declarative_base()
 
 
-import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+class State(Base):
+    '''class defination of a table'''
+    __tablename__ = 'states'
 
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                        sys.argv[1], sys.argv[2], sys.argv[3]),
-                        pool_pre_ping=True
-                    )
-    Session = sessionmaker(bind=engine)
-    Base.metadata.create_all(engine)
-
-    session = Session()
-
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-
-    session.close()
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
