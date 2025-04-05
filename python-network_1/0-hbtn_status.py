bb4571e09c14b3dcd688509c981e7ea6f343564d
selenai9 <s.isimbi2@alustudent.com>
2025-04-05 19:50:17 +0000

    dger

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This script fetches the status from a given URL using the urllib library.
+"""
+
+
+import urllib.request
+
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
+
+if __name__ == '__main__':
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This script makes a request and displays a
+response using the requests package.
+"""
+
+import requests
+
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
+
+
+# if __name__ == "__main__":
+#     response = requests.get("https://alu-intranet.hbtn.io/status")
+
+#     print("Body response:")
+#     print(f"\t- type: {type(response.text)}")
+#     print(f"\t- content: {response.text}")
